ca21a87e8789f75417eca33b8759f76c6e1273f4
gouravlinux <2005gouravsoni@gmail.com>
2026-02-13 08:42:32 +0530

    Solved stock qn with txn fees using tabulation

--- 12thFeb26 Stock Question with txn fee LC 714.java
+++ 12thFeb26 Stock Question with txn fee LC 714.java
@@ -32,3 +32,22 @@ class Solution {
         return solve(prices, fee, 0, buy);
     }
 }
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        // tabulation 
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        // dp[day][0/1] = at that day I either buy or sell stock to get maxProfit
+       
+        
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for(int i = 1;i < n;i++){
+            // dp[day][0] = I not buy or sell the existing stock
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]+prices[i]-fee);
+            // dp[day][1] = I buy or hold the existing stock
+            dp[i][1] = Math.max(dp[i-1][0]-prices[i], dp[i-1][1]); 
+        }
+        return Math.max(dp[n-1][0], dp[n-1][1]);
+    }
+}
